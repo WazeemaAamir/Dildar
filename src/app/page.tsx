8796cba035465dddd281c31d5aca1187c9d56427
WAZEEMA AAMIR <161026876+WazeemaAamir@users.noreply.github.com>
2025-08-31 21:50:34 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+import Contact from "@/components/Contact";
+import Navbar from "@/components/Navbar";
+export default function HomePage() {
+  return (
+     <div>
+    <Navbar />
+    <Hero />
+    <Contact />
+     </div>
+  );
+}
